de38b24b82158ca7060ec54560e0d0978ee86486
joyefa566 <164298128+joyefa566@users.noreply.github.com>
2024-09-12 09:22:03 +0600

    Create Joy23.js

---
+++ scripts/commands/Joy23.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "Joy23",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "😒",
+        cooldowns: 5, 
+},
+
+handleEvent: function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😬")==0) {
+		var msg = {
+				body: "যেখানে স্বপ্নয় আমাদের\nএক করতে পারে না\nসেখানে বাস্তবতা তাে নির্মম।🙂\n\n𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐄𝐃 𝐉𝐎𝐘",
+				attachment: fs.createReadStream(__dirname + `/Joy/joytt.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+
+  }
+}
