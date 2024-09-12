5cae86a903dbcd967e99a016f4466c1d5ecebf63
joyefa566 <164298128+joyefa566@users.noreply.github.com>
2024-09-12 08:17:57 +0600

    Create Efa+joy.js

---
+++ scripts/commands/Efa+joy.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "Efa+joy",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "😒",
+        cooldowns: 5, 
+},
+
+handleEvent: function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+	if (body.indexOf("tafriya")==0 || body.indexOf("Tafriya")==0 || body.indexOf("Efa")==0 || body.indexOf("efa")==0) {
+		var msg = {
+				body: "𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐄𝐃 𝐉𝐎𝐘",
+				attachment: fs.createReadStream(__dirname + `/Joy/tafriyaefa.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("💚", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+
+  }
+}
