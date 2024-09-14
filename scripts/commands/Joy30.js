cc8212824c03d7bec73fb24442eb7a102ead100c
joyefa566 <164298128+joyefa566@users.noreply.github.com>
2024-09-14 06:37:52 +0600

    Create Joy30.js

---
+++ scripts/commands/Joy30.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "Joy30",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "😒",
+        cooldowns: 5, 
+},
+
+handleEvent: function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+	if (body.indexOf(" ")==0 || body.indexOf("🖤")==0 || body.indexOf("🤭")==0 || body.indexOf("🫢")==0) {
+		var msg = {
+				body: "____𝐍𝐞𝐯𝐞𝐫 𝐜𝐡𝐚𝐧𝐠𝐞 𝐲𝐨𝐮𝐫 𝐩𝐞𝐫𝐬𝐨𝐧𝐚𝐥𝐢𝐭𝐲-!!😊/n____যে ভালোবাসার সে তোমাকে তোমার মতোই/n ভালোবাসবে-!!🖤シ/n
+♡💚😽༉༎/n/n𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐄𝐃 𝐉𝐎𝐘",
+				attachment: fs.createReadStream(__dirname + `/Joy/tafriyaefa.mp4`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+
+  }
+}
